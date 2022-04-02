73939f5cdfdc54b30f82893b18bfcc4f875439e1
Jonas Johansen <46403752+CionDeveloping@users.noreply.github.com>
2022-04-02 22:34:19 +0200

    v2.01

---
+++ sb_bunn.php
@@ -0,0 +1,8 @@
+    <!--SCOREBOARD - BUNN-->
+    <br>
+    <br>
+    <h3>Alle rettigheter reservert - Jonas Johansen</h3>
+    <a href="https://github.com/CionDeveloping/ctf_verifier"><h4>JD SYSTEMS @ 2022</h4></a>
+</center>
+</body>
+</html>
