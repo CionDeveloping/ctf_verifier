90803cd3dc676d34877a79264e220300c9b43a17
Jonas Johansen <46403752+CionDeveloping@users.noreply.github.com>
2022-04-02 22:33:50 +0200

    V2.01

---
+++ sb_topp.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <!--JOHANSEN DATA @ 2022 | SCOREBOARD SYSTEM-->
+     <!--SCOREBOARD - TOPP-->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scoreboard</title>
+    <style>
+        *{
+            font-family: sans-serif;
+        }
+        ol{
+            max-width: 200px;
+            background-color: rgb(221, 221, 221);
+            padding: 30px;
+            font-size: 20px;
+            border-radius: 20px;
+            box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+        }
+    </style>
+</head>
+<body>
+<center>
+    <h2>CTF1 Scoreboard</h2>
+    <!--SCOREBOARD - TOPP END -->
