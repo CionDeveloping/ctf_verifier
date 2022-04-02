211fdf807a07b67db8c3135d3a25517422e61778
Jonas Johansen <46403752+CionDeveloping@users.noreply.github.com>
2022-04-02 22:34:59 +0200

    V2.02

--- scoreboard.php
+++ scoreboard.php
@@ -1,5 +1,8 @@
+
+<?php include("sb_topp.php");?>
+
 <?php
-//JOHANSEN DATA @ 2022 | SCOREBOARD SYSTEM
+// JOHANSEN DATA SYSTEMS @ 2022 | SCOREBOARD V2
 $content = file('scoreboard_database.txt');
 echo '<ol>';
 foreach ($content as $line) {
@@ -7,3 +10,5 @@ foreach ($content as $line) {
 }
 echo '</ol>';
 ?>
+
+<?php include("sb_bunn.php");?>
